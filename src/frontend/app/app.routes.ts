439c115737bc1ddf25dbf17f36e84be4a7b32aea
Robert Jamróz <rockfade@gmail.com>
2026-02-23 21:22:27 +0100

    update view

--- src/frontend/app/app.routes.ts
+++ src/frontend/app/app.routes.ts
@@ -1,41 +1,50 @@
-import { Routes } from '@angular/router';
-import { authGuard } from './guards/auth.guard';
-import { LoginComponent } from './components/login/login.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { AllocationViewComponent } from './components/allocation-view/allocation-view.component';
-import { LineListComponent } from './components/line-list/line-list.component';
-import { WorkstationListComponent } from './components/workstation-list/workstation-list.component';
-import { AuditLogComponent } from './components/audit-log/audit-log.component';
-
+import { Routes } from "@angular/router";
+import { authGuard } from "./guards/auth.guard";
+import { LoginComponent } from "./components/login/login.component";
+import { ProductListComponent } from "./components/product-list/product-list.component";
+import { AllocationViewComponent } from "./components/allocation-view/allocation-view.component";
+import { LineListComponent } from "./components/line-list/line-list.component";
+import { WorkstationListComponent } from "./components/workstation-list/workstation-list.component";
+import { AuditLogComponent } from "./components/audit-log/audit-log.component";
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    {
-        path: 'products',
-        component: ProductListComponent,
-        canActivate: [authGuard]
-    },
-    {
-        path: 'allocations',
-        component: AllocationViewComponent,
-        canActivate: [authGuard]
-    },
-    {
-        path: 'lines',
-        component: LineListComponent,
-        canActivate: [authGuard]
-    },
-    {
-        path: 'workstations',
-        component: WorkstationListComponent,
-        canActivate: [authGuard]
-    },
+  { path: "login", component: LoginComponent, title: "Logowanie do systemu" },
+  {
+    path: "products",
+    component: ProductListComponent,
+    canActivate: [authGuard],
+    title: "Produkty",
+  },
+  {
+    path: "allocations",
+    component: AllocationViewComponent,
+    canActivate: [authGuard],
+    title: "Alokacje",
+  },
+  {
+    path: "lines",
+    component: LineListComponent,
+    canActivate: [authGuard],
+    title: "Linie montażowe",
+  },
+  {
+    path: "workstations",
+    component: WorkstationListComponent,
+    canActivate: [authGuard],
+    title: "Stanowiska",
+  },
 
-    {
-        path: 'audit-logs',
-        component: AuditLogComponent,
-        canActivate: [authGuard]
-    },
-    
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+  {
+    path: "audit-logs",
+    component: AuditLogComponent,
+    canActivate: [authGuard],
+    title: "Logi audytu",
+  },
+
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full",
+    title: "Logowanie do systemu",
+  },
 ];
--- src/frontend/app/app.ts
+++ src/frontend/app/app.ts
@@ -1,25 +1,53 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AuthService } from './services/auth.service';
+import { CommonModule } from "@angular/common";
+import { Component, inject, signal, effect } from "@angular/core";
+import {
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+  Router,
+  ActivatedRoute,
+  NavigationEnd,
+} from "@angular/router";
+import { AuthService } from "./services/auth.service";
+import { Title } from "@angular/platform-browser";
+import { filter, map, mergeMap } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    CommonModule,
-    RouterLink,
-    RouterLinkActive
-  ],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  selector: "app-root",
+  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: "./app.html",
+  styleUrl: "./app.scss",
 })
 export class App {
-  protected readonly title = signal('frontend');
   authService = inject(AuthService);
+  private titleService = inject(Title);
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
 
+  protected readonly currentTitle = signal("");
+
+  constructor() {
+    effect(() => {
+      this.titleService.setTitle(`${this.currentTitle()} -  ALSA`);
+    });
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map((route) => {
+          while (route.firstChild) route = route.firstChild;
+          return route;
+        }),
+        mergeMap((route) => route.title),
+      )
+      .subscribe((title) => {
+        if (title) {
+          this.currentTitle.set(title);
+        }
+      });
+  }
   onLogout() {
-    if (confirm('Czy na pewno chcesz się wylogować?')) {
+    if (confirm("Czy na pewno chcesz się wylogować?")) {
       this.authService.logout();
     }
   }
